25fb145002bae83dd2ca3cfd4b3690c58968aacb
Fernando Poumián <fpoumian@gmail.com>
2017-03-31 13:48:34 -0700

    "Next" button in index must link to route "/posts?page=<pagenumber>

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -6,5 +6,6 @@ app_name = 'posts'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<slug>[\w-]+)/$', views.detail, name='detail'),
+    url(r'^posts/', views.index, name='list'),
+    url(r'^posts/(?P<slug>[\w-]+)/$', views.detail, name='detail'),
 ]
