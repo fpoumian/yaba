541843cf64aa70caff1591fd360dec4f9cc33b5b
Fernando Poumián <fpoumian@gmail.com>
2017-03-31 18:57:42 -0700

    Add custom templates for 404 and 500 errors.

--- src/yaba/urls.py
+++ src/yaba/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -25,6 +26,8 @@ urlpatterns = [
                   url(r'^about/', views.about, name='about'),
                   url(r'^contact/', views.contact, name='contact'),
                   url(r'^markdownx/', include('markdownx.urls')),
+                  url(r'^404/$', TemplateView.as_view(template_name='yaba/404.html')),
+                  url(r'^500/$', TemplateView.as_view(template_name='yaba/500.html')),
                   url(r'^', include('posts.urls')),
               ] \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
