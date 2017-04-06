91d5fbca68432a6ad3ef0767ed79ca6a5ecc5cf1
Fernando Poumián <fpoumian@gmail.com>
2017-04-06 12:10:28 -0600

    Add conditional rendering of external services and social media buttons.

--- src/yaba_core/context_processors.py
+++ src/yaba_core/context_processors.py
@@ -3,7 +3,10 @@ from django.conf import settings
 
 def global_settings(request):
     return {
-        'LAST_FM_API_KEY': settings.LAST_FM_API_KEY,
-        'LAST_FM_USERNAME': settings.LAST_FM_USERNAME,
-        'LAST_FM_SCROBBLES_LIMIT': settings.LAST_FM_SCROBBLES_LIMIT
+        'LAST_FM_API_KEY': getattr(settings, 'LAST_FM_API_KEY', None),
+        'LAST_FM_USERNAME': getattr(settings, 'LAST_FM_USERNAME', None),
+        'LAST_FM_SCROBBLES_LIMIT': getattr(settings, 'LAST_FM_SCROBBLES_LIMIT', 10),
+        'TWITTER_USERNAME': getattr(settings, 'TWITTER_USERNAME', None),
+        'GITHUB_USERNAME': getattr(settings, 'GITHUB_USERNAME', None),
+        'DISQUS_WEBSITE_SHORTNAME': getattr(settings, 'DISQUS_WEBSITE_SHORTNAME', None),
     }
