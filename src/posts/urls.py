5910b4efcc975779d53530184e8f5e1d930b9fc4
Fernando Poumián <fpoumian@gmail.com>
2017-04-06 13:36:49 -0600

    Replace function based views for Index and Posts list

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -7,8 +7,8 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^posts/$', views.list, name='list'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^posts/$', views.PostsList.as_view(), name='list'),
     url(r'^posts/(?P<slug>[\w-]+)/$', views.detail, name='detail'),
     url(r'^posts/tag/(?P<tag>[^/]+(?u))/$',
         views.TaggedPostsList.as_view(),
--- src/posts/views.py
+++ src/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .utils import get_posts_from_page, render_post_list
 from tagging.views import TaggedObjectList
+from django.views.generic import ListView
 
 from .models import Post
 
@@ -10,6 +11,31 @@ def index(request):
     page = request.GET.get('page')
 
     return render_post_list(request, get_posts_from_page(page))
+
+
+class PostsList(ListView):
+    template_name = 'posts/list.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+    model = Post
+    queryset = Post.published_objects.all()
+
+    def get(self, request, *args, **kwargs):
+        page = request.GET.get('page')
+
+        # If a page parameter is not present in url
+        # let the index view handle the request
+        # so that "/posts/" route redirects to  "/"
+        if not page:
+            return redirect('posts:index', permanent=True)
+        return super().get(self, request, *args, **kwargs)
+
+
+class IndexView(PostsList):
+    def get(self, request, *args, **kwargs):
+        # return the get() method from grandparent class (ListView)
+        # to avoid infinite redirects
+        return ListView.get(self, request, *args, **kwargs)
 
 
 def list(request):
