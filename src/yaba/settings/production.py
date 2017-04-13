0351b505a16db4b13262511ec44e9d8f0663d430
Fernando Poumián <fpoumian@gmail.com>
2017-04-12 20:53:40 -0600

    Change name of boto storage class.

--- src/yaba/settings/production.py
+++ src/yaba/settings/production.py
@@ -27,8 +27,8 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 # S3 File Storage
 INSTALLED_APPS += ('storages',)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'poumian-yaba'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
