ef834397e564b336dc7ee37d1c73016445508a38
Fernando Poumián <fpoumian@gmail.com>
2017-04-13 12:59:34 -0600

    Fix publicPath for fonts in Webpack config.

--- src/yaba/settings/local.py
+++ src/yaba/settings/local.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Development Email Server
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'testing@example.com'
