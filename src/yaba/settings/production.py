cc1952a034a7ecbca02a5d0205f2711ccd3c8399
Fernando Poumián <fpoumian@gmail.com>
2017-04-14 12:40:16 -0600

    Env variables now are accessed using get_env_variable custom function.

--- src/yaba/settings/production.py
+++ src/yaba/settings/production.py
@@ -11,7 +11,6 @@ EMAIL_HOST_USER = os.getenv('GMAIL_SMTP_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('GMAIL_SMTP_HOST_PASSWORD')
 EMAIL_PORT = 587
 
-
 # Allowed Hosts
 ALLOWED_HOSTS = [
     'ec2-54-175-168-118.compute-1.amazonaws.com'
@@ -21,17 +20,17 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('RDS_DB_NAME'),
-        'USER': os.getenv('RDS_USERNAME'),
-        'PASSWORD': os.getenv('RDS_PASSWORD'),
-        'HOST': os.getenv('RDS_HOSTNAME'),
-        'PORT': os.getenv('RDS_PORT'),
+        'NAME': get_env_variable('RDS_DB_NAME'),
+        'USER': get_env_variable('RDS_USERNAME'),
+        'PASSWORD': get_env_variable('RDS_PASSWORD'),
+        'HOST': get_env_variable('RDS_HOSTNAME'),
+        'PORT': get_env_variable('RDS_PORT'),
     }
 }
 
 # AWS
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
 
 # S3 Storage
 INSTALLED_APPS += ('storages',)
